63d6bfbda393fbba24e16d86e60e07428e4798c8
ShishirBhandari <shishirbhandari54@gmail.com>
2020-09-25 16:23:53 +0545

    Add functions for hash generation and verification

---
+++ api/security.js
@@ -0,0 +1,24 @@
+const { SHA256, HmacSHA256 } = require('crypto-js');
+
+const { SECURITY_KEY, TRANSCRIPTS } = require('./data');
+
+const getHash = (message) => {
+  const hash = HmacSHA256(message, SECURITY_KEY).toString();
+
+  return hash;
+};
+
+const verifyHash = (message, hash) => {
+  const actualHash = HmacSHA256(message, SECURITY_KEY).toString();
+
+  if(actualHash == hash) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+module.exports = {
+  getHash,
+  verifyHash
+}
