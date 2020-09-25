a733f0dc74eced813ec70dfd8fc34fd6beee0463
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-09-25 16:27:39 +0545

    Add layout for different pages functionalities

--- client/src/views/Order.js
+++ client/src/views/Order.js
@@ -3,8 +3,39 @@ import React from "react";
 class Header extends React.Component {
   render() {
     return (
-      <main className="container mx-auto px-3 pb-16">
+      <main
+        className="container mx-auto px-3 pb-16"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <h1 className="text-3xl font-bold">Order Transcript</h1>
+        <hr />
+        <form className="w-full max-w-sm" style={{ marginTop: "50px" }}>
+          <div className="flex items-center border-b border-teal-500 py-2">
+            <input
+              className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="text"
+              placeholder="Enter your username"
+              aria-label="Full name"
+            />
+            <button
+              className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+              type="button"
+            >
+              Submit
+            </button>
+            <button
+              className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
+              type="button"
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
       </main>
     );
   }
