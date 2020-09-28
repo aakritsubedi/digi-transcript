97d8279424b038e2d82d6cecd56084a2ceb9674d
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-09-28 14:23:39 +0545

    Add copy to clipboard functionalities in View Order View

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,19 +1,44 @@
 import React, { useState, useEffect } from "react";
 
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
 import OrderService from "services/transcript.service";
+
+import "react-notifications/lib/notifications.css";
 
 function Home() {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const allOrders =  await OrderService.fetchAllOrders();
+      const allOrders = await OrderService.fetchAllOrders();
 
       setOrders(allOrders);
     }
-    
+
     fetchData();
   });
+
+  const displayInfo = (id) => {
+    
+    const myCertificate = {};
+    myCertificate["certificate"] = orders[id];
+
+    var textField = document.createElement("textarea");
+    textField.innerText = JSON.stringify(myCertificate);
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand("copy");
+    textField.remove();
+
+    NotificationManager.success(
+      JSON.stringify(myCertificate),
+      "Certificate Info",
+      3500
+    );
+  };
 
   return (
     <main
@@ -34,7 +59,6 @@ function Home() {
             <th className="border px-4 py-2">Student Name</th>
             <th className="border px-4 py-2">Student Score</th>
             <th className="border px-4 py-2">Transcript Hash</th>
-            <th className="border px-4 py-2">School Id</th>
             <th className="border px-4 py-2">Status</th>
             <th className="border px-4 py-2">Actions</th>
           </tr>
@@ -44,25 +68,31 @@ function Home() {
             return (
               <tr key={index}>
                 <td className="border px-4 py-2">{order.id}</td>
-                <td className="border px-4 py-2">{order.transcript.username}</td>
-                <td className="border px-4 py-2">{order.transcript.fullname}</td>
-                <td className="border px-4 py-2">{order.transcript.score}</td>
                 <td className="border px-4 py-2">
-                  {order.security.hash}
+                  {order.transcript.username}
                 </td>
-                <td className="border px-4 py-2">125</td>
+                <td className="border px-4 py-2">
+                  {order.transcript.fullname}
+                </td>
+                <td className="border px-4 py-2">{order.transcript.score}</td>
+                <td className="border px-4 py-2">{order.security.hash}</td>
                 <td className="border px-4 py-2">Pending</td>
                 <td
                   className="border px-4 py-2"
                   style={{ textAlign: "center" }}
                 >
-                  <i className="fa fa-eye"></i>
+                  <i
+                    className="fa fa-copy"
+                    title='Copy Certificate Info to clipboard'
+                    onClick={() => displayInfo(order.id)}
+                  ></i>
                 </td>
               </tr>
             );
           })}
         </tbody>
       </table>
+      <NotificationContainer />
     </main>
   );
 }
